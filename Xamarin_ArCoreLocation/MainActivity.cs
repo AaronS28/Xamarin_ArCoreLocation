6203237be69f429225254bf7cd36444dad7383ba
Rod Galdeano <rgaldeano@silentiumapps.com>
2019-10-31 12:29:50 -0300

    Segunda version del sample, estable pero no abre la camara

--- Xamarin_ArCoreLocation/MainActivity.cs
+++ Xamarin_ArCoreLocation/MainActivity.cs
@@ -21,12 +21,13 @@ using Java.Lang;
 namespace Xamarin_ArCoreLocation
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, IBiFunction, Scene.IOnUpdateListener, ILocationNodeRender
     {
         ArSceneView arSceneView;
 
         // Renderables for this example
         ViewRenderable exampleLayoutRenderable;
+        CompletableFuture exampleLayout;
 
         // Our ARCore-Location scene
         LocationScene locationScene = null;
@@ -39,10 +40,9 @@ namespace Xamarin_ArCoreLocation
             SetContentView(Resource.Layout.activity_main);
             arSceneView = (ArSceneView)FindViewById(Resource.Id.ar_scene_view);
 
-            CompletableFuture exampleLayout = ViewRenderable.InvokeBuilder().SetView(this, Resource.Layout.example_layout).Build();
-            IBiFunction renderable = new RenderableGetter(ref exampleLayoutRenderable, ref exampleLayout);
-            exampleLayout.Handle(renderable);
-            arSceneView.Scene.AddOnUpdateListener(new OnUpdateListener(locationScene, arSceneView, this, this, exampleLayoutRenderable));
+            exampleLayout = ViewRenderable.InvokeBuilder().SetView(this, Resource.Layout.example_layout).Build();
+            exampleLayout.Handle(this);
+            arSceneView.Scene.AddOnUpdateListener(this);
             ARLocationPermissionHelper.RequestPermission(this);
         }
 
@@ -51,6 +51,26 @@ namespace Xamarin_ArCoreLocation
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    if (!ARLocationPermissionHelper.HasPermission(this))
+        //    {
+        //        if (!ARLocationPermissionHelper.ShouldShowRequestPermissionRationale(this))
+        //        {
+        //            // Permission denied with checking "Do not ask again".
+        //            ARLocationPermissionHelper.LaunchPermissionSettings(this);
+        //        }
+        //        else
+        //        {
+        //            Toast.MakeText(this, "Camera permission is needed to run this application", ToastLength.Long).Show();
+        //        }
+
+        //        Finish();
+        //    }
+
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
 
         protected override void OnResume()
         {
@@ -89,52 +109,30 @@ namespace Xamarin_ArCoreLocation
             base.OnDestroy();
             arSceneView.Destroy();
         }
-	}
 
-    class LocationNodeRender : ILocationNodeRender
-    {
-        ViewRenderable exampleLayoutRenderable;
-
-        public LocationNodeRender(ref ViewRenderable layoutRenderable)
+        private Node getExampleView()
         {
-            exampleLayoutRenderable = layoutRenderable;
-        }
-
-        public IntPtr Handle => Thread.CurrentThread().Handle;
-
-        public void Dispose()
-        {
-        }
-
-        public void Render(LocationNode node)
-        {
+            Node baseNode = new Node();
+            baseNode.Renderable = exampleLayoutRenderable;
+            // Add  listeners etc here
             View eView = exampleLayoutRenderable.View;
-            TextView distanceTextView = (TextView)eView.FindViewById(Resource.Id.textView2);
-            distanceTextView.SetText(node.Distance);
-        }
-    }
-
-    class OnUpdateListener : Google.AR.Sceneform.Scene.IOnUpdateListener
-    {
-        LocationScene locationScene;
-        ArSceneView arSceneView;
-        Context context;
-        Activity activity;
-        ViewRenderable exampleLayoutRenderable;
-
-        public OnUpdateListener(LocationScene loc, ArSceneView arsc, Context cont, Activity act, ViewRenderable vr)
-        {
-            locationScene = loc;
-            arSceneView = arsc;
-            context = cont;
-            activity = act;
-            exampleLayoutRenderable = vr;
+            return baseNode;
         }
 
-        public IntPtr Handle => Thread.CurrentThread().Handle;
-
-        public void Dispose()
+        public Java.Lang.Object Apply(Java.Lang.Object t, Java.Lang.Object u)
         {
+            if (u != null)
+            {
+                return null;
+            }
+
+            try
+            {
+                exampleLayoutRenderable = (ViewRenderable)exampleLayout.Get();
+            }
+            catch (System.Exception) { }
+
+            return null;
         }
 
         public void OnUpdate(FrameTime p0)
@@ -143,7 +141,7 @@ namespace Xamarin_ArCoreLocation
             {
                 // If our locationScene object hasn't been setup yet, this is a good time to do it
                 // We know that here, the AR components have been initiated.
-                locationScene = new LocationScene(context, activity, arSceneView);
+                locationScene = new LocationScene(this, this, arSceneView);
 
                 // Now lets create our location markers.
                 // First, a layout
@@ -151,8 +149,7 @@ namespace Xamarin_ArCoreLocation
 
                 // An example "onRender" event, called every frame
                 // Updates the layout with the markers distance
-                layoutLocationMarker.RenderEvent = new LocationNodeRender(ref exampleLayoutRenderable);
-
+                layoutLocationMarker.RenderEvent = this;
                 // Adding the marker
                 locationScene.MLocationMarkers.Add(layoutLocationMarker);
 
@@ -175,64 +172,12 @@ namespace Xamarin_ArCoreLocation
             return;
         }
 
-        private Node getExampleView()
+        public void Render(LocationNode node)
         {
-            Node baseNode = new Node();
-            baseNode.Renderable = exampleLayoutRenderable;
-            // Add  listeners etc here
             View eView = exampleLayoutRenderable.View;
-            return baseNode;
+            TextView distanceTextView = (TextView)eView.FindViewById(Resource.Id.textView2);
+            distanceTextView.SetText(node.Distance);
         }
     }
-
-    class RenderableGetter : IBiFunction
-    {
-        ViewRenderable exampleLayoutRenderable;
-        CompletableFuture exampleLayout;
-
-        public RenderableGetter(ref ViewRenderable vr, ref CompletableFuture cf)
-        {
-            exampleLayoutRenderable = vr;
-            exampleLayout = cf;
-        }
-
-        public IntPtr Handle => throw new NotImplementedException();
-
-        public Java.Lang.Object Apply(Java.Lang.Object t, Java.Lang.Object u)
-        {
-            if (u != null)
-            {
-                return null;
-            }
-
-            try
-            {
-                exampleLayoutRenderable = (ViewRenderable)exampleLayout.Get();
-            }
-            catch (System.Exception) {}
-
-            return null;
-        }
-
-        public void Dispose()
-        {
-        }
-    }
-
-    class BiFunctionTest : IBiFunction
-    {
-        public IntPtr Handle => throw new NotImplementedException();
-
-        public Java.Lang.Object Apply(Java.Lang.Object t, Java.Lang.Object u)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
 }
 
