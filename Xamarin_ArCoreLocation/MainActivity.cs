9689239043d5c17a1124e12e4e31809ff4c8fd91
Rod Galdeano <rgaldeano@silentiumapps.com>
2019-10-31 15:58:57 -0300

    Tercera version, estable y funcionando, pero necesita mas calibracion para ubicar los puntos geograficos

--- Xamarin_ArCoreLocation/MainActivity.cs
+++ Xamarin_ArCoreLocation/MainActivity.cs
@@ -23,12 +23,13 @@ namespace Xamarin_ArCoreLocation
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, IBiFunction, Scene.IOnUpdateListener, ILocationNodeRender
     {
+        bool installRequested;
+        bool hasFinishedLoading = false;
+        Snackbar loadingMessageSnackbar = null;
         ArSceneView arSceneView;
-
         // Renderables for this example
         ViewRenderable exampleLayoutRenderable;
         CompletableFuture exampleLayout;
-
         // Our ARCore-Location scene
         LocationScene locationScene = null;
 
@@ -41,36 +42,30 @@ namespace Xamarin_ArCoreLocation
             arSceneView = (ArSceneView)FindViewById(Resource.Id.ar_scene_view);
 
             exampleLayout = ViewRenderable.InvokeBuilder().SetView(this, Resource.Layout.example_layout).Build();
-            exampleLayout.Handle(this);
+            CompletableFuture.AllOf(exampleLayout).Handle(this);
             arSceneView.Scene.AddOnUpdateListener(this);
             ARLocationPermissionHelper.RequestPermission(this);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (!ARLocationPermissionHelper.HasPermission(this))
+            {
+                if (!ARLocationPermissionHelper.ShouldShowRequestPermissionRationale(this))
+                {
+                    // Permission denied with checking "Do not ask again".
+                    ARLocationPermissionHelper.LaunchPermissionSettings(this);
+                }
+                else
+                {
+                    Toast.MakeText(this, "Camera permission is needed to run this application", ToastLength.Long).Show();
+                }
+
+                Finish();
+            }
+
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-
-        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        //{
-        //    if (!ARLocationPermissionHelper.HasPermission(this))
-        //    {
-        //        if (!ARLocationPermissionHelper.ShouldShowRequestPermissionRationale(this))
-        //        {
-        //            // Permission denied with checking "Do not ask again".
-        //            ARLocationPermissionHelper.LaunchPermissionSettings(this);
-        //        }
-        //        else
-        //        {
-        //            Toast.MakeText(this, "Camera permission is needed to run this application", ToastLength.Long).Show();
-        //        }
-
-        //        Finish();
-        //    }
-
-        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        //}
 
         protected override void OnResume()
         {
@@ -79,6 +74,29 @@ namespace Xamarin_ArCoreLocation
             if (locationScene != null)
             {
                 locationScene.Resume();
+            }
+
+            if (arSceneView.Session == null)
+            {
+                // If the session wasn't created yet, don't resume rendering.
+                // This can happen if ARCore needs to be updated or permissions are not granted yet.
+                try
+                {
+                    Session session = CreateArSession(this, installRequested);
+                    if (session == null)
+                    {
+                        installRequested = ARLocationPermissionHelper.HasPermission(this);
+                        return;
+                    }
+                    else
+                    {
+                        arSceneView.SetupSession(session);
+                    }
+                }
+                catch (UnavailableException e)
+                {
+                    //DemoUtils.handleSessionException(this, e);
+                }
             }
 
             try
@@ -110,12 +128,34 @@ namespace Xamarin_ArCoreLocation
             arSceneView.Destroy();
         }
 
+        public static Session CreateArSession(Activity activity, bool installRequested)
+        {
+            Session session = null;
+            // if we have the camera permission, create the session
+            if (ARLocationPermissionHelper.HasPermission(activity)) 
+            {
+                //switch (ArCoreApk.Instance.RequestInstall(activity, !installRequested)) 
+                //{
+                //    case "INSTALL_REQUESTED":
+                //        return null;
+                //    case "INSTALLED":
+                //        break;
+                //}
+                session = new Session(activity);
+                // IMPORTANT!!!  ArSceneView needs to use the non-blocking update mode.
+                Config config = new Config(session);
+                config.SetUpdateMode(Config.UpdateMode.LatestCameraImage);
+                session.Configure(config);
+            }
+
+            return session;
+        }
+
         private Node getExampleView()
         {
             Node baseNode = new Node();
             baseNode.Renderable = exampleLayoutRenderable;
             // Add  listeners etc here
-            View eView = exampleLayoutRenderable.View;
             return baseNode;
         }
 
@@ -129,6 +169,7 @@ namespace Xamarin_ArCoreLocation
             try
             {
                 exampleLayoutRenderable = (ViewRenderable)exampleLayout.Get();
+                hasFinishedLoading = true;
             }
             catch (System.Exception) { }
 
@@ -137,6 +178,11 @@ namespace Xamarin_ArCoreLocation
 
         public void OnUpdate(FrameTime p0)
         {
+            if (!hasFinishedLoading)
+            {
+                return;
+            }
+
             if (locationScene == null)
             {
                 // If our locationScene object hasn't been setup yet, this is a good time to do it
@@ -145,30 +191,31 @@ namespace Xamarin_ArCoreLocation
 
                 // Now lets create our location markers.
                 // First, a layout
-                LocationMarker layoutLocationMarker = new LocationMarker(-65.410517, -24.787947, getExampleView());
+                LocationMarker layoutLocationMarker = new LocationMarker(-0.119677, 51.478494, getExampleView());
 
                 // An example "onRender" event, called every frame
                 // Updates the layout with the markers distance
                 layoutLocationMarker.RenderEvent = this;
                 // Adding the marker
                 locationScene.MLocationMarkers.Add(layoutLocationMarker);
-
-                Frame frame = arSceneView.ArFrame;
-                if (frame == null)
-                {
-                    return;
-                }
-
-                if (frame.Camera.TrackingState != TrackingState.Tracking)
-                {
-                    return;
-                }
-
-                if (locationScene != null)
-                {
-                    locationScene.ProcessFrame(frame);
-                }
             }
+
+            Frame frame = arSceneView.ArFrame;
+            if (frame == null)
+            {
+                return;
+            }
+
+            if (frame.Camera.TrackingState != TrackingState.Tracking)
+            {
+                return;
+            }
+
+            if (locationScene != null)
+            {
+                locationScene.ProcessFrame(frame);
+            }
+
             return;
         }
 
@@ -176,7 +223,7 @@ namespace Xamarin_ArCoreLocation
         {
             View eView = exampleLayoutRenderable.View;
             TextView distanceTextView = (TextView)eView.FindViewById(Resource.Id.textView2);
-            distanceTextView.SetText(node.Distance);
+            distanceTextView.SetText(node.Distance.ToString(), TextView.BufferType.Normal);
         }
     }
 }
